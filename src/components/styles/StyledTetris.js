a20918e6aa770603c8afe1d38bbde3960462955e
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-11 12:37:16 -0600

    Adds aside styling to StyledTetris

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -16,4 +16,11 @@ export const StyledTetris = styled.div`
     padding: 40px;
     margin: 0 auto;
     max-width: 900px;
+
+    aside {
+        width: 100%;
+        max-width: block;
+        display: block;
+        padding: 0 20px;
+    }
 `

abc3eb17fe1a3a1c0c16fd88dfaf03fdbd2f2cb8
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-12 13:15:01 -0600

    Finishes code in usePlayer.js

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -4,13 +4,21 @@ import { useState } from 'react'
 import { randomTetromino } from '../tetrominos'
 
 export const usePlayer = () => {
-    const [player, setPlayer] = useState()
+    const [player, setPlayer] = useState({
+        pos: { x: 0, y: 0},
+        tetromino: randomTetromino().shape,
+        collided: false,
+    })
 
-    // above is shorthand for vvv
-    // const playerState = useState()
-    // const player = playerState[0]
-    // const setPlayer = playerState[1]
+    return [player]
 }
 
 
 // name custom hooks === 'use'_____ so react will know it's a custom hook
+
+
+//const [player, setPlayer] = useState()
+// above is shorthand for vvv
+// const playerState = useState()
+// const player = playerState[0]
+// const setPlayer = playerState[1]

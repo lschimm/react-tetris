4169e57225e75ebb9d4280d58abc3ad10756fc86
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-13 10:43:31 -0600

    Codes out useStage.js

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // Functions
 import { createStage } from '../gameHelpers'
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+
+    useEffect(() => {
+        const updateStage = prevStage => {
+            // first clear the stage
+            const newStage = prevStage.map(row => 
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+                )
+            // Then draw the tetromino
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    //check which cells are occupied//know shape of tetromino
+                    if(value !== 0) {
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            value,
+                            `${player.collided ? 'merged' : 'clear'}`
+                        ]
+                    }
+                })
+            })
+            return newStage
+        }
+            
+        setStage(prev => updateStage(prev))
+
+    }, [player.collided, player.pos.x, player.pos.y, player.tetromino])
   
     return [stage, setStage];
   }

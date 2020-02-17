b812e4c866c1ba7d4b3252b238264754dc554ae3
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-17 10:26:59 -0600

    Start checkCollision function in gameHelpers

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -7,3 +7,25 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, 'clear'])
   )
+
+
+
+// for loop will be faster than map or for each for this
+// won't be able to break out of the loop once we collide with something with foreach
+
+export const checkCollision = (player, stage, { x: moveX, y:moveY }) => {
+  // loop through tetromino
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[0].length; x += 1 ) {
+      // first check if we're on tetromino cell // check it isn't 0
+      if (player.tetromino[y][x] !== 0) {
+        // Check tetromino move is inside height of the stage (y)
+
+        // Check tetromino move is inside the width of the stage (x)
+
+        // Check that the cell isn't set to 'clear'
+
+      }
+    }
+  }
+}

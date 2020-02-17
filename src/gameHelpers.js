61aad2328127898c40889fb344a22bb2400ec571
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-17 10:31:43 -0600

    Finishes checkCollision

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -19,12 +19,16 @@ export const checkCollision = (player, stage, { x: moveX, y:moveY }) => {
     for (let x = 0; x < player.tetromino[0].length; x += 1 ) {
       // first check if we're on tetromino cell // check it isn't 0
       if (player.tetromino[y][x] !== 0) {
+        if (
         // Check tetromino move is inside height of the stage (y)
-
+        !stage[y + player.pos.y + moveY] ||
         // Check tetromino move is inside the width of the stage (x)
-
+        !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
         // Check that the cell isn't set to 'clear'
-
+        stage[y +  player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+        ) {
+          return true
+        }
       }
     }
   }

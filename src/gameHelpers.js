b7b315fdf51211a5df266f9b03b2ee34001f4e69
Leilani <Leilani@Tais-MacBook-Pro.local>
2020-02-17 10:52:51 -0600

    fixes bug on line 26 in tetris.js

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -13,10 +13,9 @@ export const createStage = () =>
 // for loop will be faster than map or for each for this
 // won't be able to break out of the loop once we collide with something with foreach
 
-export const checkCollision = (player, stage, { x: moveX, y:moveY }) => {
-  // loop through tetromino
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y += 1) {
-    for (let x = 0; x < player.tetromino[0].length; x += 1 ) {
+    for (let x = 0; x < player.tetromino[y].length; x += 1) {
       // first check if we're on tetromino cell // check it isn't 0
       if (player.tetromino[y][x] !== 0) {
         if (
@@ -25,8 +24,9 @@ export const checkCollision = (player, stage, { x: moveX, y:moveY }) => {
         // Check tetromino move is inside the width of the stage (x)
         !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
         // Check that the cell isn't set to 'clear'
-        stage[y +  player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
-        ) {
+        stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+        'clear'
+        )  {
           return true
         }
       }
